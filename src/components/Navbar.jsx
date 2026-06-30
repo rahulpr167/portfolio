51792a03c17a6c26c0438bb2b608dfadf0f66d6c
Vishnu K B <vkb4477@gmail.com>
2026-06-30 20:14:11 +0530

    revert: restore Navbar to original (no Contact in desktop nav)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           <li><a href="#about"    onClick={(e) => scrollTo(e, 'about')}>About</a></li>
           <li><a href="#skills"   onClick={(e) => scrollTo(e, 'skills')}>Skills</a></li>
           <li><a href="#projects" onClick={(e) => scrollTo(e, 'projects')}>Projects</a></li>
-          <li><a href="#contact"  onClick={(e) => scrollTo(e, 'contact')}>Contact</a></li>
+         
           
         </ul>
 
